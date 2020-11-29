fdbfd7585d9de404cf2690745369b3e9edd12d5a
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-29 04:36:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ class Breakfast{
     this.drink = drink
   }
 };
-Create a class for Lunch. Lunch will have a constructor with a salad, a soup, and a drink.
+
+//2.Create a class for Lunch. Lunch will have a constructor with a salad, a soup, and a drink.
 Create a class for Dinner. Since dinner is a little bit fancier, Dinner will have a constructor with salad, soup, entree, and dessert. Indicate that dessert is intended to be a private property by including an underscore (_) at the beginning.
