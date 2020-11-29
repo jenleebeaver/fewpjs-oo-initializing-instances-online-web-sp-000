af986e09eb2d5c91f6e79e6bce04542362c17c31
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-29 04:39:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ class Lunch{
   }
 };
 
-//3.Create a class for Dinner. Since dinner is a little bit fancier, Dinner will have a constructor with salad, soup, entree, and dessert. 
+//3.Create a class for Dinner. Since dinner is a little bit fancier, Dinner will have a constructor with salad, soup, entree, and dessert.
 //Indicate that dessert is intended to be a private property by including an underscore (_) at the beginning.
 class Dinner{
   constructor(salad, soup, entree, dessert{
-    
+
   }  
 };
