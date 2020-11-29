cee4334e1b82c2524508d0feb06740aa4a191ec9
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-29 04:40:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ class Lunch{
 //Indicate that dessert is intended to be a private property by including an underscore (_) at the beginning.
 class Dinner{
   constructor(salad, soup, entree, dessert{
-
-  }  
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
 };
